f368e85ac95c3160012bd9f524f469aa73147fa7
Swank <willmadedesign@gmail.com>
2022-09-05 17:30:21 -0700

    Add slider, choose color & remove grid functions

--- script.js
+++ script.js
@@ -11,20 +11,55 @@ function createGrid() {
     }
 };
 
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+const slider = document.querySelector('#slider');
+const slideVal = document.querySelector('.value');
+slider.addEventListener('input', function(){
+    let val = document.getElementById('slider').value;
+    slideVal.textContent = val;
+    removeAllChildNodes(grid);
+    grid.setAttribute('style', `grid-template-columns: repeat(${val}), 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    for (i = 0; i < val * val; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        grid.appendChild(div)
+    }
+});
+
+
 const reset = document.querySelector('#reset');
 reset.addEventListener('click', function(){
     let val = document.getElementById('slider').value;
     let cell = grid.children;
-    for (i = 0; i < val * val; i++) {
+    for (i = 0; i < val*val; i++) {
         cell[i].style.backgroundColor = 'white';
     }
 });
 
 const black = document.querySelector('#black');
 black.addEventListener('click', function(){
-
+    
 })
 const rainbow = document.querySelector('#rainbow');
 
+const chooseColor = document.querySelector('#color');
+chooseColor.addEventListener('input', function() {
+    let val = document.getElementById('slider').value;
+    let newColor = document.getElementById('color').value;
+    let cell = grid.children;
+    for (i = 0; i < val*val; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = newColor;
+        })
+    }
+});
 
 createGrid();
